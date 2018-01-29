cee64dd9c34aefa3f6ad2b8c8cc2d98ba5beac94
lockin02 <smallno@qq.com>
2018-01-29 18:09:45 +0800

    修改广告控制器方法
    未测试

--- App/Admin/Controller/AdController.class.php
+++ App/Admin/Controller/AdController.class.php
@@ -63,51 +63,70 @@ class AdController extends ComController {
 
     public function _before_add(){
         $regionList = $this->_hotel_list();
+        $hid_condition = $this->isHotelMenber();
+        $map = [];
+        if ($hid_condition) {   
+            $map['hid'] = $hid_condition;
+        }
+        $regionList = $this->_new_hotel_list($map);
         $this->assign('menuTree', $regionList);
     }
-    
+
     public function edit(){
-        $Ad = D(CONTROLLER_NAME);
         if(count($_REQUEST['ids'])!=1){
             $this->error("只能修改一条记录");
             die();
         }
-        $regionList = $this->_hotel_list();
+        $bind_list = array();   //绑定hid数据集合
+        $hidsArr = array(); //hid集合
         $id = $_REQUEST['ids'][0];
-        $list = $Ad->getById($id);
-        $ids = trim($list['hidlist']);
-        $idsArr = explode(",", $ids);
-        foreach ($regionList as $key => $value) {
-            if(!empty($value['sub'])){ 
-                foreach ($value['sub'] as $kk => $vv) {
-                    if(in_array($vv['hid'], $idsArr)){
-                        $value['sub'][$kk]['isselect'] = 1;
-                    }else{
-                        $value['sub'][$kk]['isselect'] = 0;
+        $list = D("ad")->where('id='.$id)->find();
+        if ($hid_condition) {   
+            $map['hid'] = $hid_condition;
+        }
+        $map = [];
+        $hid_condition = $this->isHotelMenber();
+        if ($hid_condition) {   
+            $map['hid'] = $hid_condition;
+        }
+        $regionList = $this->_new_hotel_list($map);
+        if ($regionList) {
+            $bind_list = D("ad_bind")->where('ad_id='.$id)->field('hid')->select();
+            foreach ($bind_list as $key => $value) {
+                $hidsArr[] = $value['hid'];
+            }
+            foreach ($regionList as $key => $value) {
+                if(!empty($value['sub'])){ 
+                    foreach ($value['sub'] as $kk => $vv) {
+                        if(in_array($vv['hid'], $hidsArr)){
+                            $value['sub'][$kk]['isselect'] = 1;
+                        }else{
+                            $value['sub'][$kk]['isselect'] = 0;
+                        }
                     }
                 }
+                $regionList[$key] = $value;
             }
-            $regionList[$key] = $value;
         }
+
         $this->assign('menuTree', $regionList);
-        if (empty($list['hidlist'])) {
+        if (empty($bind_list)) {
             $list['hotelname']='/';
-        }else if($list['hidlist']=="all"){
-            $list['hotelname']="全部酒店";
         }else{
-            
             $hotelname='';
-            $Hotel = D("Hotel");
-            foreach ($idsArr as $value) {
-                $vo=array();
-                $vo=$Hotel->getByHid($value);
-                $hotelname = $hotelname.','.$vo['hotelname'];
+            $hotel_where['hid'] = array('in',$hidsArr);
+            $hotelnamelist = D("hotel")->where($hotel_where)->field('hotelname')->select();
+            if (!empty($hotelnamelist)) {
+                foreach ($hotelnamelist as $key => $value) {
+                    $hotelname_arr[] = $value['hotelname'];
+                }
+                $list['hotelname'] = implode(",", $hotelname_arr);
             }
-            $list['hotelname']=trim($hotelname,',');
         }
         $this->assign("vo",$list);
         $this->display();
     }
+
     public function detail(){
         $model = M("ad");
         $list = $model->getById($_REQUEST['id']);
@@ -129,6 +148,7 @@ class AdController extends ComController {
         }
         echo json_encode($list);
     }
+
     //保存
     public function update(){
         $model = M(CONTROLLER_NAME);
@@ -136,7 +156,8 @@ class AdController extends ComController {
         $data['title'] = I('post.title','','strip_tags');
         $data['filepath'] = I('post.filepath','','strip_tags');
         $data['audit_status'] = 0;
-        $data['hidlist'] = trim(I('post.hidlist','','strip_tags'));
+        $hidlist = trim(I('post.hidlist','','strip_tags'));
+        $passhid_arr = explode(",", $hidlist);
         $size = I('post.size','','intval');
         $data['size'] = round($size/1024,3);
         if (empty($data['filepath'])) {
@@ -144,85 +165,68 @@ class AdController extends ComController {
         }
         $vo = '';
         $model->startTrans();
+
         if($data['id']){
+            // 获取广告信息
             $vo = $model->getById($data['id']);
             if ($data['filepath'] != $vo['filepath']) {
-                $data['upload_time']=time();
+                $data['upload_time'] = time();
             }
             $result = $model->data($data)->where('id='.$data['id'])->save();
-            addlog('修改广告图片信息，ID：'.$data['id']);
+            // 获取绑定hid列表
+            $bind_hidlist = D("ad_bind")->where('ad_id='.$data['id'])->field('hid')->select();
+            $bind_hid_arr = [];
+            if (!empty($bind_hidlist)) {
+                foreach ($bind_hidlist as $key => $value) {
+                    $bind_hid_arr[] = $value['hid'];
+                }
+            }
+            
+            $add_hid = array_diff($passhid_arr, $bind_hid_arr);// 新增hid
+            $del_hid = array_diff($bind_hid_arr, $passhid_arr);// 删除hid
+            
+            // 新增绑定表
+            if (!empty($add_hid)) {
+                foreach ($add_hid as $key => $value) {
+                    $add_bind_info[$key]['ad_id'] = $data['id'];
+                    $add_bind_info[$key]['hid'] = $value;
+                }
+                try {
+                    D('ad_bind')->addAll($add_bind_info);
+                } catch (Exception $e) {
+                    $model->rollback();
+                    $this->error('操作失败！',U('index'));
+                }
+            }
+            // 删除绑定表
+            if (!empty($del_hid)) {
+                $del_bind_where['ad_id'] = $data['id'];
+                $del_bind_where['hid'] = array('in', $del_hid);
+                try {
+                    D('ad_bind')->where($del_bind_where)->delete();
+                } catch (Exception $e) {
+                    $model->rollback();
+                    $this->error('操作失败！',U('index'));
+                }
+            }
+            $editvolumeresult = $this->editsize($data['id'],$del_hid);
         }else{
-            $data['upload_time']=time();
+            $data['upload_time'] = time();
             $result = $model->data($data)->add();
-            addlog('添加广告图片，ID：'.$result);
-        }
-        //直接更改容量
-        if($data['id']){
-            $changesize = $data['size'] - $vo['size'];
-            if($data['hidlist'] == $vo['hidlist']){//没更改酒店
-                if($data['hidlist'] == 'all'){
-                    $arrhid = M("hotel_volume")->field('hid')->select();
-                    $arr = array();
-                    foreach ($arrhid as $key => $value) {
-                        array_push($arr, $value['hid']);
-                    }
-                    $arrmap['hid'] = array('in',$arr);
-                    $updatesize = M("hotel_volume")->where($arrmap)->setInc('ad_size',$changesize);
-                }else{
-                    $amap['hid'] = array('in',$data['hidlist']);
-                    $updatesize = M("hotel_volume")->where($amap)->setInc('ad_size',$changesize);
-                }
-
-            }else{//更改酒店
-                if($vo['hidlist'] == 'all'){//本来是全部酒店
-                    $arrhid = M("hotel_volume")->field('hid')->select();
-                    $arr = array();
-                    foreach ($arrhid as $key => $value) {
-                        array_push($arr, $value['hid']);
-                    }
-                    $arrmap['hid'] = array('in',$arr);
-                    $dresult = M("hotel_volume")->where($arrmap)->setDec('ad_size',$vo['size']);
-                    $imap['hid'] = $data['hidlist'];
-                    $iresult = M("hotel_volume")->where($imap)->setInc('ad_size',$data['size']);
-
-                }else{//本来不是全部酒店
-                    $dmap['hid'] = array('in',$vo['hidlist']);
-                    $dresult = M("hotel_volume")->where($dmap)->setDec('ad_size',$vo['size']);
-                    if($data['hidlist'] == 'all'){//更改为全部酒店
-                        $arrhid = M("hotel_volume")->field('hid')->select();
-                        $arr = array();
-                        foreach ($arrhid as $key => $value) {
-                            array_push($arr, $value['hid']);
-                        }
-                        $arrmap['hid'] = array('in',$arr);
-                        $iresult = M("hotel_volume")->where($arrmap)->setInc('ad_size',$data['size']);
-                    }else{ 
-                        $imap['hid'] = array('in',$data['hidlist']);
-                        $iresult = M("hotel_volume")->where($imap)->setInc('ad_size',$data['size']);
-                    }
-                }
-                if($iresult !== false && $dresult !== false){
-                    $updatesize = true;
-                }else{
-                    $updatesize = false;
-                }
+            // 新增绑定表
+            foreach ($passhid_arr as $key => $value) {
+                $add_bind_info[$key]['ad_id'] = $result;
+                $add_bind_info[$key]['hid'] = $value;
             }
-        }else{//新增
-            if($data['hidlist'] == 'all'){
-                $arrhid = M("hotel_volume")->field('hid')->select();
-                $arr = array();
-                foreach ($arrhid as $key => $value) {
-                    array_push($arr, $value['hid']);
-                }
-                $arrmap['hid'] = array('in',$arr);
-                $updatesize = D("hotel_volume")->where($arrmap)->setInc('ad_size',$data['size']);
-            }else{
-                $amap['hid'] = array('in',$data['hidlist']);
-                $updatesize = M("hotel_volume")->where($amap)->setInc('ad_size',$data['size']);
+            try {
+                D('ad_bind')->addAll($add_bind_info);  
+            } catch (Exception $e) {
+                $this->error('操作失败！',U('index'));
             }
-
+            $editvolumeresult = $this->addsize($data['size'],$passhid_arr);
         }
-        if($result !== false && $updatesize !== false){
+       
+        if($result !== false && $editvolumeresult !== false){
             if(!empty($vo)){
                 if ($data['filepath'] != $vo['filepath']) {
                     @unlink(FILE_UPLOAD_ROOTPATH.$vo['filepath']);
@@ -235,6 +239,56 @@ class AdController extends ComController {
             $this->error('操作失败！',U('index'));
         }
     }
+
+    private function addsize($nowsize, $nowhid = array()){
+        if (empty($nowhid)) {
+            $where_hid['hid'] = array('in', $nowhid);
+            try {
+                D("hotel_volume")->where($where_hid)->setInc('ad_size',$nowsize);
+            } catch (Exception $e) {
+                return false;
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function editsize($id=11, $diffhid = array()){
+        if (!$id) {
+            return false;
+        }
+        $sql = "SELECT hid, GROUP_CONCAT(ad_id) as id_str FROM `zxt_ad_bind` WHERE hid in (SELECT hid FROM `zxt_ad_bind` WHERE ad_id = $id) ";
+        if (!empty($diffhid)) {
+            $sql .= " OR hid in (".implode(",", $diffhid).")";
+        }
+        $sql .= " GROUP BY hid";
+        $bindlist = D("ad_bind")->query($sql);
+        if (!empty($bindlist)) {
+            foreach ($bindlist as $key => $value) {
+                $where_id = $value['id_str'];
+                $current_hid = $value['hid'];
+                $hid_arr[] = $value['hid'];
+                $sql_ad = "SELECT $current_hid as current_hid, SUM(size) as current_size FROM `zxt_ad` WHERE id IN($where_id)";
+                $sizelist = D('ad')->query($sql_ad);
+                $update_info[] = $sizelist['0'];
+            }
+            $sql_volume = "UPDATE `zxt_hotel_volume` SET ad_size = CASE";
+            foreach ($update_info as $key => $value) {
+                $sql_volume .= sprintf(" WHEN '%s' THEN %s", $value['current_hid'], $value['current_size']);
+            }
+            $hid_str = implode(",", $hid_arr);
+            $sql_volume .= " END WHERE hid IN($hid_str)";
+            try {
+                D('hotel_volume')->execute($sql_volume);
+            } catch (Exception $e) {
+                return false;
+            }
+            return true;
+        }
+
+    }
+
     public function delete(){
         $model = M(CONTROLLER_NAME);
         $ids = isset($_REQUEST['ids'])?$_REQUEST['ids']:false;
@@ -280,6 +334,7 @@ class AdController extends ComController {
             $this->error('删除失败，参数错误！');
         }
     }
+
     public function upload(){
         $callback = array();
         if (!empty($_FILES[$_REQUEST["name"]]["name"])) {
